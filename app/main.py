2ea66abbcc2534b50fa05c34a642cade8f06216a
srivatsa2026 <srivatsa.is22@bmsce.ac.in>
2025-03-09 19:02:36 +0530

    Nothing Changed

--- app/main.py
+++ app/main.py
@@ -22,8 +22,8 @@ app.include_router(post.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
-      
-  
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
